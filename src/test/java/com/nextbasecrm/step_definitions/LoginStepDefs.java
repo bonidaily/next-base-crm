d6ebf7cd678d77070930fd737ac1097508fab054
bonidaily <arbonshehu@gmail.com>
2024-05-07 16:39:44 +0200

    Created BasePage and created methods to call pages and modules

--- src/test/java/com/nextbasecrm/step_definitions/LoginStepDefs.java
+++ src/test/java/com/nextbasecrm/step_definitions/LoginStepDefs.java
@@ -1,11 +1,14 @@
 package com.nextbasecrm.step_definitions;
 
+import com.nextbasecrm.pages.BasePage;
 import com.nextbasecrm.pages.LoginPage;
+import com.nextbasecrm.utilities.BrowserUtils;
 import com.nextbasecrm.utilities.ConfigurationReader;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import org.apache.commons.logging.Log;
 
-public class LoginStepDefs {
+public class LoginStepDefs extends BasePage {
     LoginPage loginPage = new LoginPage();
 
 
@@ -17,27 +20,27 @@ public class LoginStepDefs {
     @Given("the user logged in as {string}")
     public void the_user_logged_in_as(String userType) {
         //based on input enter that user information
-        String username =null;
-        String password =null;
+        String username = null;
+        String password = null;
 
-        if(userType.equalsIgnoreCase("hr1")){
+        if (userType.equalsIgnoreCase("hr1")) {
             username = ConfigurationReader.getProperty("hr1_username");
             password = ConfigurationReader.getProperty("hr1_password");
-        }else if(userType.equalsIgnoreCase("helpdesk")){
+        } else if (userType.equalsIgnoreCase("helpdesk")) {
             username = ConfigurationReader.getProperty("helpdesk1_username");
             password = ConfigurationReader.getProperty("helpdesk1_password");
-        }else if(userType.equalsIgnoreCase("marketing")){
+        } else if (userType.equalsIgnoreCase("marketing")) {
             username = ConfigurationReader.getProperty("marketing1_username");
             password = ConfigurationReader.getProperty("marketing1_password");
         }
         //send username and password and login
-        new LoginPage().login(username,password);
+        new LoginPage().login(username, password);
     }
 
     @Given("the user logged in with username as {string} and password as {string}")
     public void the_user_logged_in_with_username_as_and_password_as(String username, String password) {
-      LoginPage loginPage=new LoginPage();
-      loginPage.login(username,password);
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(username, password);
     }
 
 
@@ -47,4 +50,7 @@ public class LoginStepDefs {
         loginPage.login1();
 
     }
+
+
 }
+
